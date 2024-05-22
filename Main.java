82225210bd6e3d0cd317f908713c28ebe2a9ac78
Trevor <trevordmooney@gmail.com>
2024-05-22 14:31:45 -0700

    Updated main file with integer function

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int integer1 = 10;
+        int integer2 = 20;
+        int sum = integer1 + integer2;
+        System.out.printf("%d + %d = %d", integer1, integer2, sum);
+
+
+    }
+}
